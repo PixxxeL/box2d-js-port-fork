9ede7ee9045f3d38aa22a017d26e73cc4e44b297
PixxxeL <piksel@mail.ru>
2014-08-09 23:31:37 +0600

    Replace origin html file with list of files with list in builder

--- builder.py
+++ builder.py
@@ -1,16 +1,80 @@
 
-import re
 import os
 
 
 VERBOSE_YUI = False
-YUI_APP_PATH = 'd:\\www\\yuicompressor-2.4.8.jar'
+YUI_APP_PATH = os.path.join('d:\\', 'www', 'yuicompressor-2.4.8.jar')
 SOURCE = ('origin', 'fork',)[1]
 MIN_SUFF = 'min'
-ORIGIN_HTML_PATH = 'data\\list-of-files.html'
-ORIGIN_PRE_URL_REPLACES = ['js/', 'src/%s/' % SOURCE]
-RESULT_PATH = 'src\\%s\\box2d.js' % SOURCE
-RESULT_MIN_PATH = 'src\\%s\\box2d.min.js' % SOURCE
+LIB_URL_PREFIX = os.path.join('src', '%s' % SOURCE)
+RESULT_PATH = os.path.join(LIB_URL_PREFIX, 'box2d.js')
+RESULT_MIN_PATH = os.path.join(LIB_URL_PREFIX, 'box2d.min.js')
+LIBLIST = (
+    'box2d/common/b2Settings.js',
+    'box2d/common/math/b2Vec2.js',
+    'box2d/common/math/b2Mat22.js',
+    'box2d/common/math/b2Math.js',
+    'box2d/collision/b2AABB.js',
+    'box2d/collision/b2Bound.js',
+    'box2d/collision/b2BoundValues.js',
+    'box2d/collision/b2Pair.js',
+    'box2d/collision/b2PairCallback.js',
+    'box2d/collision/b2BufferedPair.js',
+    'box2d/collision/b2PairManager.js',
+    'box2d/collision/b2BroadPhase.js',
+    'box2d/collision/b2Collision.js',
+    'box2d/collision/Features.js',
+    'box2d/collision/b2ContactID.js',
+    'box2d/collision/b2ContactPoint.js',
+    'box2d/collision/b2Distance.js',
+    'box2d/collision/b2Manifold.js',
+    'box2d/collision/b2OBB.js',
+    'box2d/collision/b2Proxy.js',
+    'box2d/collision/ClipVertex.js',
+    'box2d/collision/shapes/b2Shape.js',
+    'box2d/collision/shapes/b2ShapeDef.js',
+    'box2d/collision/shapes/b2BoxDef.js',
+    'box2d/collision/shapes/b2CircleDef.js',
+    'box2d/collision/shapes/b2CircleShape.js',
+    'box2d/collision/shapes/b2MassData.js',
+    'box2d/collision/shapes/b2PolyDef.js',
+    'box2d/collision/shapes/b2PolyShape.js',
+    'box2d/dynamics/b2Body.js',
+    'box2d/dynamics/b2BodyDef.js',
+    'box2d/dynamics/b2CollisionFilter.js',
+    'box2d/dynamics/b2Island.js',
+    'box2d/dynamics/b2TimeStep.js',
+    'box2d/dynamics/contacts/b2ContactNode.js',
+    'box2d/dynamics/contacts/b2Contact.js',
+    'box2d/dynamics/contacts/b2ContactConstraint.js',
+    'box2d/dynamics/contacts/b2ContactConstraintPoint.js',
+    'box2d/dynamics/contacts/b2ContactRegister.js',
+    'box2d/dynamics/contacts/b2ContactSolver.js',
+    'box2d/dynamics/contacts/b2CircleContact.js',
+    'box2d/dynamics/contacts/b2Conservative.js',
+    'box2d/dynamics/contacts/b2NullContact.js',
+    'box2d/dynamics/contacts/b2PolyAndCircleContact.js',
+    'box2d/dynamics/contacts/b2PolyContact.js',
+    'box2d/dynamics/b2ContactManager.js',
+    'box2d/dynamics/b2World.js',
+    'box2d/dynamics/b2WorldListener.js',
+    'box2d/dynamics/joints/b2JointNode.js',
+    'box2d/dynamics/joints/b2Joint.js',
+    'box2d/dynamics/joints/b2JointDef.js',
+    'box2d/dynamics/joints/b2DistanceJoint.js',
+    'box2d/dynamics/joints/b2DistanceJointDef.js',
+    'box2d/dynamics/joints/b2Jacobian.js',
+    'box2d/dynamics/joints/b2GearJoint.js',
+    'box2d/dynamics/joints/b2GearJointDef.js',
+    'box2d/dynamics/joints/b2MouseJoint.js',
+    'box2d/dynamics/joints/b2MouseJointDef.js',
+    'box2d/dynamics/joints/b2PrismaticJoint.js',
+    'box2d/dynamics/joints/b2PrismaticJointDef.js',
+    'box2d/dynamics/joints/b2PulleyJoint.js',
+    'box2d/dynamics/joints/b2PulleyJointDef.js',
+    'box2d/dynamics/joints/b2RevoluteJoint.js',
+    'box2d/dynamics/joints/b2RevoluteJointDef.js',
+)
 
 def _yui_compress(src, dst):
     cmd = [
@@ -34,7 +98,7 @@ def _read(path):
 def _proc_src(src):
     data = []
     min_data = []
-    src = src.replace(*ORIGIN_PRE_URL_REPLACES)
+    src = os.path.join(LIB_URL_PREFIX, src)
     dst = src.split('.')
     dst.insert(-1, MIN_SUFF)
     dst = '.'.join( dst )
@@ -55,18 +119,12 @@ def _proc_dst(src, dst, data, min_data):
     os.remove(dst)
 
 def main():
-    src_re = re.compile(r'<script src=\'([^\']+)\'')
     if os.path.isfile(RESULT_PATH):
         os.remove(RESULT_PATH)
     if os.path.isfile(RESULT_MIN_PATH):
         os.remove(RESULT_MIN_PATH)
-    data = _read(ORIGIN_HTML_PATH)
-    items = src_re.findall(data)
-    #if SOURCE == 'fork':
-    #    items.append('js/box2d/extensions/b2World.js')
-    for item in items:
-        if item.startswith('js/'):
-            _proc_src(item)
+    for item in LIBLIST:
+        _proc_src(item.replace('/', os.sep))
 
 
 if __name__ == '__main__':
