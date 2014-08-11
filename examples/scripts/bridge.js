80f04734e3a5d5e9d3b4bdf11cc75b5d25638423
PixxxeL <piksel@mail.ru>
2014-08-11 21:10:34 +0600

    Add bridge example functionality

--- examples/scripts/bridge.js
+++ examples/scripts/bridge.js
@@ -2,44 +2,78 @@
 var bridge;
 
 bridge = function() {
-  var body, ground, i, rjd, world, _i, _results;
+  var body, ground, i, p0, p1, p2, p3, p4, pm, points, prevBody, rjd, world, _i, _j, _k, _l, _results;
   world = window.world;
   ground = world.GetGroundBody();
   rjd = new b2RevoluteJointDef;
   rjd.lowerAngle = -15 / TO_RAD;
   rjd.upperAngle = 15 / TO_RAD;
   rjd.enableLimit = true;
-  _results = [];
+  prevBody = ground;
   for (i = _i = 0; _i <= 9; i = ++_i) {
-    _results.push(body = addBody({
-      name: "plank " + i,
+    body = addBody({
+      name: "plank " + (i + 1),
+      x: 122 + 44 * i,
+      y: 250,
       width: 48,
       height: 10,
       density: 20,
       friction: .2
+    });
+    rjd.Initialize(prevBody, body, new b2Vec2(100 + 44 * i, 250));
+    world.CreateJoint(rjd);
+    prevBody = body;
+  }
+  rjd.Initialize(prevBody, ground, new b2Vec2(100 + 44 * 10, 250));
+  world.CreateJoint(rjd);
+  for (i = _j = 0; _j <= 4; i = ++_j) {
+    addBody({
+      name: "box " + (i + 1),
+      x: Math.random() * 400 + 120,
+      y: Math.random() * 150 + 50,
+      width: Math.random() * 10 + 20,
+      height: Math.random() * 10 + 20,
+      density: 1,
+      friction: .3,
+      restitution: .1
+    });
+  }
+  for (i = _k = 0; _k <= 4; i = ++_k) {
+    addBody({
+      name: "circle " + (i + 1),
+      x: Math.random() * 400 + 120,
+      y: Math.random() * 150 + 50,
+      radius: Math.random() * 5 + 10,
+      density: 1,
+      friction: .3,
+      restitution: .1
+    });
+  }
+  _results = [];
+  for (i = _l = 0; _l <= 14; i = ++_l) {
+    if (Math.random() > .66) {
+      points = [[-10 - Math.random() * 10, 10 + Math.random() * 10], [-5 - Math.random() * 10, -10 - Math.random() * 10], [5 + Math.random() * 10, -10 - Math.random() * 10], [10 + Math.random() * 10, 10 + Math.random() * 10]];
+    } else if (Math.random() > .5) {
+      pm = Math.random() * .5 + .8;
+      p0 = [0, 10 + Math.random() * 10];
+      p2 = [-5 - Math.random() * 10, -10 - Math.random() * 10];
+      p3 = [5 + Math.random() * 10, -10 - Math.random() * 10];
+      p1 = [(p0[0] + p2[0]) * pm, (p0[1] + p2[1]) * pm];
+      p4 = [(p3[0] + p0[0]) * pm, (p3[1] + p0[1]) * pm];
+      points = [p0, p1, p2, p3, p4];
+    } else {
+      points = [[0, 10 + Math.random() * 10], [-5 - Math.random() * 10, -10 - Math.random() * 10], [5 + Math.random() * 10, -10 - Math.random() * 10]];
+    }
+    _results.push(addBody({
+      name: "polygon " + (i + 1),
+      x: Math.random() * 400 + 120,
+      y: Math.random() * 150 + 50,
+      points: points,
+      density: 1,
+      friction: .3,
+      restitution: .1,
+      rotation: Math.random() * Math.PI
     }));
   }
   return _results;
-
-  /*
-  var ;
-  
-  var prevBody:b2Body = ground;
-  for (i = 0; i < numPlanks; ++i)
-  {
-      bd.position.Set((100 + 22 + 44 * i) / m_physScale, 250 / m_physScale);
-      body = m_world.CreateBody(bd);
-      body.CreateFixture(fixtureDef);
-      
-      anchor.Set((100 + 44 * i) / m_physScale, 250 / m_physScale);
-      jd.Initialize(prevBody, body, anchor);
-      m_world.CreateJoint(jd);
-      
-      prevBody = body;
-  }
-  
-  anchor.Set((100 + 44 * numPlanks) / m_physScale, 250 / m_physScale);
-  jd.Initialize(prevBody, ground, anchor);
-  m_world.CreateJoint(jd);
-   */
 };
